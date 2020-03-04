25d4a23233daf718bb1f94cde411c913ae0701f2
guihunkun <615815574@qq.com>
2020-03-04 21:41:57 +0800

    exercise_02

---
+++ exercise_02/changeString.py
@@ -0,0 +1,10 @@
+'''
+定义一个变量sentence，赋值为"I Love you!"，然后分别用title(), upper(), lower()函数对变量sentence执行操作后输出。
+'''
+print("\n")
+
+
+
+
+
+
---
+++ exercise_02/name.py
@@ -0,0 +1,10 @@
+'''
+将Robert Downey分别存在两个变量中，再合并成一个变量，打印一句话: Robert Downey says "I'm the Iron Man."
+'''
+print("\n")
+
+
+
+
+
+
