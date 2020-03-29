e2b46ca86734d26de170680526d9e91700d48bc1
guihunkun <615815574@qq.com>
2020-03-29 21:09:19 +0800

    solution_04

---
+++ solution_04/numbers-finished.py
@@ -0,0 +1,12 @@
+'''
+借助for循环打印数字3～16（含）
+'''
+print("\n")
+
+for value in range(3,17):
+	print(value)
+
+
+
+
+
---
+++ solution_04/slice-finished.py
@@ -0,0 +1,12 @@
+'''
+定义一个列表为players，里面存如下信息['charles', 'martina', 'michael', 'florence', 'eli']，然后分别切片打印列表中的前两个人名；最后一个人名。
+'''
+print("\n")
+
+players=['charles','martina','michael','florence','eli']
+print(players[0:2])
+print(players[-1:])
+
+
+
+
